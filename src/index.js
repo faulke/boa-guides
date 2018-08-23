acc50cb35944fa942051f5211f626d6f00344391
Evan Faulk <faulkev@gmail.com>
2018-08-22 21:38:04 -0700

    Add react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
-const Index = () => {
-  return <div>Hello React!</div>;
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+),
+  document.getElementById("index")
+)
